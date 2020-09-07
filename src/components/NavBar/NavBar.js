a00a7e525c9211148f7393a185eca5b2aa8d44a0
RyanWolfen7 <wolfen7lada@gmail.com>
2020-09-07 08:48:53 -0500

    made SideBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavContainer, Title, Hamburger } from '../../styles/NavBarStyles'
 
 const NavBar = props => {
+    const [navBar, setNavBar ] = useState(false)
 
     return (
         <NavContainer>
---
+++ src/components/NavBar/SideBar.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const SideBar = props => {
+
+    return <div> </div>
+}
+
+export default SideBar
